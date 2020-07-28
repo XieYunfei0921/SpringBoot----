e31efaa340477b0989db66139b1cf708fdf7b50e
XieYunfei0921 <XieYunfei0921@126.com>
2020-07-28 17:37:35 +0800

    Rest服务

--- src/main/java/com/example/demo/service/MyRestService.java
+++ src/main/java/com/example/demo/service/MyRestService.java
@@ -1,12 +1,15 @@
 package com.example.demo.service;
 
+import com.example.demo.dao.BookReposity;
 import com.example.demo.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class MyRestService {
+
 	private final RestTemplate restTemplate;
 
 	public MyRestService(RestTemplateBuilder restTemplateBuilder) {
