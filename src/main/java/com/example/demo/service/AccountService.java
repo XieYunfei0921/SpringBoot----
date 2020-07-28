bad5b4e9859a5557a5229093a7f026f9a3b79e78
XieYunfei0921 <XieYunfei0921@126.com>
2020-07-28 17:37:42 +0800

    账户服务

--- src/main/java/com/example/demo/service/AccountService.java
+++ src/main/java/com/example/demo/service/AccountService.java
@@ -1,4 +1,10 @@
 package com.example.demo.service;
 
+import com.example.demo.dao.BookReposity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 public interface AccountService {
+
+
 }
